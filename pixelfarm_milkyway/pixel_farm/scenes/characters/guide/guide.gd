35e7628d6b600072a4ecbd1b985d8d621323d5e2
sm02-08 <shelleyma02@gmail.com>
2026-01-03 23:32:49 -0500

    working on dialogue button
    edited dialogue button -- stylized it to be more suited towards the game's theme (used GD extension dialogue manager)

--- pixelfarm_milkyway/pixel_farm/scenes/characters/guide/guide.gd
+++ pixelfarm_milkyway/pixel_farm/scenes/characters/guide/guide.gd
@@ -1,17 +1,31 @@
 extends Node2D
 # ohh so to get rid of the constant issue of having the func _ready(): pass already loaded in and stuff, just disable template... why did i never try this before.
 
+# to make the "e" actually interact with the player, put an input map 
+var balloon_scene = preload("res://dialogue/game_dialogue_balloon.tscn")
+
 @onready var interactable_component: InteractableComponent = $InteractableComponent
 @onready var interactable_label_component: Control = $InteractableLabelComponent
 
+var in_range: bool 
 
 func _ready() -> void: 
 	interactable_component.interactable_activated.connect(on_interactable_activated)
 	interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
 	interactable_label_component.hide() 
 	
-func on_interactable_activated() -> void: 
-	interactable_label_component.show() 
+func on_interactable_activated() -> void:
+	interactable_label_component.show()
+	in_range = true 
 	
 func on_interactable_deactivated() -> void: 
 	interactable_label_component.hide() 
+	in_range = false 
+
+func _unhandled_input(event: InputEvent) -> void: 
+	if in_range: 
+		if event.is_action_pressed("show_dialogue"): 
+			var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate() 
+			get_tree().current_scene.add_child(balloon) 
+			balloon.start(load("res://dialogue/conversations/guide.dialogue"), "start")
+		 
--- pixelfarm_milkyway/pixel_farm/scenes/components/interactable_component.gd
+++ pixelfarm_milkyway/pixel_farm/scenes/components/interactable_component.gd
@@ -4,15 +4,6 @@ extends Area2D
 signal interactable_activated 
 signal interactable_deactivated 
 
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#pass
-
 # these two allow for the player to walk *through* the door instead of colliding with it 
 #func _on_body_entered(body):
 	#interactable_activated.emit() 
@@ -25,3 +16,11 @@ signal interactable_deactivated
 # layer 1 = objects, layer 2 = player, layer 3 = interactables 
 # on collision, this is layer 3 
 # you select mask layer 2 so that it interacts with player (layer 2) 
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	interactable_activated.emit()
+
+
+func _on_body_exited(_body: Node2D) -> void:
+	interactable_deactivated.emit()
